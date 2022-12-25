da3407a9791e4bc62d6c3762c5e9984a6770c2b9
zezer0 <xornjs1423@gmail.com>
2022-12-25 21:33:40 +0900

    initialize `GithubActionTest`

---
+++ githubActionTest/GithubActionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubActionTest
+//
+//  Created by 재영신 on 2022/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
